0b5980c7e8537c000262e3e36d80b06585b6346d
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-18 16:38:57 +0800

    背景和全局样式

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,22 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+html{
+  height: 100%;
+}
+body {
+  height: 100%;
+  background: url('./assets/background.png') no-repeat;
+}
+#app{
   color: #2c3e50;
-  margin-top: 60px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  width: 600px;
+  position: absolute;
+  left: 50%;
+  top: 35%;
+  margin-left: -300px;
+  margin-top: -100px;
+  text-align: center;
 }
 </style>
