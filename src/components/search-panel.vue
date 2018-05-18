6ccc2cbbfa40ecef113e282bdaca445a1ad869a2
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-18 18:13:39 +0800

    搜索框组件

--- src/components/search-panel.vue
+++ src/components/search-panel.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    <logo-select />
-    I'm select-panel
+  <div class="panel">
+    <logo-select @getIndex="getIndex"/>
+        <el-autocomplete
+          class="inline-input"
+          v-model="inputValue"
+          :fetch-suggestions="querySearch"
+          placeholder="请输入内容"
+          :trigger-on-focus="false"
+          @select="selectDataList"
+          prefix-icon="el-icon-search"
+        >
+          <i slot="suffix" class="el-input__icon el-icon-close" v-show="inputValue" @click="clearInput"></i>
+          <div slot-scope="{ item }">{{ item }}</div>
+        </el-autocomplete>
+        <el-button type="success" @click="search">搜索</el-button>
   </div>
 </template>
 
@@ -15,11 +27,60 @@ import logoSelect from './logo-select'
       'logo-select':logoSelect,
     },
     data:() => ({
-          show: true,
-        })
+      inputValue:'',
+      selectIndex:0,
+      myData:[],
+      logoData: [ {
+              name: '百度搜索',
+              searchSrc: 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd='
+          }, {
+              name: '360搜索',
+              searchSrc: 'https://www.so.com/s?ie=utf-8&shb=1&src=360sou_newhome&q='
+          }, {
+              name: '搜狗搜索',
+              searchSrc: 'https://www.sogou.com/web?query='
+          }]
+        }),
+    methods:{
+      getIndex(index){
+        console.log(index);
+        this.selectIndex = index;
+      },
+      querySearch(queryString,cb){
+        this.$http.jsonp('https://sug.so.360.cn/suggest?word=' + queryString + '&encodein=utf-8&encodeout=utf-8').then(res => {
+                this.myData = res.data.s;
+                cb(this.myData);
+        }).catch(error => {console.log(error);})
+      },
+      search(){
+        window.location.href = this.logoData[this.selectIndex].searchSrc + this.inputValue;
+        this.inputValue = '';
+      },
+      clearInput(){
+        this.inputValue = '';
+      },
+      selectDataList(item){
+        this.inputValue = item;
+      }
+    }
   }
 </script>
 
 <style type="text/css">
-
+.el-icon-close{
+  cursor: pointer;
+}
+.el-input {
+  width:500px;
+}
+.el-input__inner{
+  height: 50px;
+}
+.el-button {
+  width: 90px;
+  height: 50px;
+}
+.panel{
+  font-size: 0;
+}
 </style>
